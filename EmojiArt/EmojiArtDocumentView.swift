5113881157744f5be849504d8d0d73be07982e3d
stleye <stleye@gmail.com>
2022-07-17 10:08:04 -0300

    [Lecture 14] Added ScaledMetric to font size

--- EmojiArt/EmojiArtDocumentView.swift
+++ EmojiArt/EmojiArtDocumentView.swift
@@ -10,9 +10,9 @@ import SwiftUI
 struct EmojiArtDocumentView: View {
     
     @ObservedObject var document: EmojiArtDocument
-    
-    let defaultEmojiFontSize = CGFloat(40)
-    
+
+    @ScaledMetric var defaultEmojiFontSize = CGFloat(40)
+
     var body: some View {
         VStack(spacing: 0) {
             documentBody
